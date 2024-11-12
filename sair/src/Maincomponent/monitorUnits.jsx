931ab8b7b818385c2f454874da571a7e41fe13b7
Jumanahali <443200866@student.ksu.edu.sa>
2024-11-12 19:08:21 +0300

    crash function

--- sair/src/Maincomponent/monitorUnits.jsx
+++ sair/src/Maincomponent/monitorUnits.jsx
@@ -41,36 +41,32 @@ export const monitorUnits = (sess, fetchInterval) => {
 
 
 
-  const fetchUnits = async () => {
-    console.log('Fetching units...');
-    const flags = window.wialon.item.Item.dataFlag.base | window.wialon.item.Unit.dataFlag.lastMessage;
-
-    try {
-      const code = await new Promise((resolve) => {
-        sess.updateDataFlags([{ type: 'type', data: 'avl_unit', flags: flags, mode: 0 }], resolve);
-      });
-
-      if (code) {
-        console.log(window.wialon.core.Errors.getErrorText(code));
-        return;
-      }
-
-      const loadedUnits = sess.getItems('avl_unit');
-      if (loadedUnits) {
-        console.log('Loaded units:', loadedUnits);
-        loadedUnits.forEach(element => {
-            console.log(element.getName());
+      const fetchUnits = async () => {
+        const flags = window.wialon.item.Item.dataFlag.base | window.wialon.item.Unit.dataFlag.lastMessage;
+        try {
+          const code = await new Promise((resolve) => {
+            sess.updateDataFlags([{ type: 'type', data: 'avl_unit', flags: flags, mode: 0 }], resolve);
           });
-        await processUnits(loadedUnits);
-      } else {
-        console.log('No units found.');
-      }
-    } catch (error) {
-      console.error('Error fetching units:', error);
-    }
-  };
+    
+          if (code) {
+            return;
+          }
+          const loadedUnits = sess.getItems('avl_unit');
+          if (loadedUnits) {
+            await processUnits1(loadedUnits);
+            await processUnits2(loadedUnits);
+    
+          } else {
+            console.log('No units found.');
+          }
+        } catch (error) {
+          console.error('Error fetching units:', error);
+        }
+      };
 
-  const processUnits = async (units) => {
+  // violation function
+
+  const processUnits1 = async (units) => {
     for (const unit of units) { //change foreach to for loop
         const pos = unit.getPosition();
         console.log(pos);
@@ -78,12 +74,12 @@ export const monitorUnits = (sess, fetchInterval) => {
         if(pos){
           const GPSserialnumber = unit.getName();
           console.log('gpsnum:',GPSserialnumber);
-          const maxSpeed = await fetchMaxSpeed(pos.y,pos.x); 
+          const maxSpeed = 160;//await fetchMaxSpeed(pos.y,pos.x); 
           console.log('Max speed from API in proccess method:', maxSpeed);
 
         
        if (maxSpeed!==0) {
-        const driverSpeed = pos.s;
+        const driverSpeed = 175;//pos.s;
         console.log('driverspeed:',driverSpeed);
         if (driverSpeed > maxSpeed) {
          const driverQuerySnapshot = await getDocs(
@@ -95,14 +91,80 @@ export const monitorUnits = (sess, fetchInterval) => {
          if (!driverQuerySnapshot.empty) {
           const driverid = driverQuerySnapshot.docs[0].data().DriverID;
           console.log('DriverID:', driverid);
-          const price = calculatePrice(driverSpeed, maxSpeed);
+          var price = calculatePrice(driverSpeed, maxSpeed);
           console.log('price:',price);
           if(price!==0){
+            const querySnapshot3 = await getDocs(
+              query(
+                collection(db, 'Motorcycle'),
+                where('GPSnumber', '==', GPSserialnumber)
+              )
+            );
+            if (!querySnapshot3.empty) {
+         const Brand = querySnapshot3.docs[0].data().Brand;
+         const LicensePlate = querySnapshot3.docs[0].data().LicensePlate;
+         const Model = querySnapshot3.docs[0].data().Model;
+         const MotorcycleID = querySnapshot3.docs[0].data().MotorcycleID;
+         const Type = querySnapshot3.docs[0].data().Type;
+         const ViolationID = generateViolationId(); 
          const newViolationTime=unit.getLastMessage().rt;//time of this violation
          console.log('viotime:',newViolationTime);
+         var count50=0; //count if there is reckless violation when maxspeed<=120
+         var count30=0; //count if there is reckless violation when maxspeed>120
+         if (maxSpeed <= 120){
+          if(price==1500){
+            count50=1;
+            const starttime = newViolationTime - 365 * 24 * 60 * 60;// one year
+            const endtime = newViolationTime;
+            const querySnapshot1 = await getDocs(
+              query(
+                collection(db, 'Violation'),
+                where('GPSnumber', '==', GPSserialnumber),
+                where('driverID', '==', driverid),
+                where('time', '>=', starttime),
+                where('time', '<=', endtime)
+              )
+            );
+            if (!querySnapshot1.empty){
+              querySnapshot1.forEach((doc) => {
+              const violationData = doc.data();
+              count50 += violationData.count50;
+            });
+            }
+            if(count50>1){
+              price=2000;//max limit price for violation
+             }
+          }
+         }
+         else{
+          if(price==1500){
+            count30=1;
+            const starttime = newViolationTime - 365 * 24 * 60 * 60;// one year
+            const endtime = newViolationTime;
+            const querySnapshot2 = await getDocs(
+              query(
+                collection(db, 'Violation'),
+                where('GPSnumber', '==', GPSserialnumber),
+                where('driverID', '==', driverid),
+                where('time', '>=', starttime),
+                where('time', '<=', endtime)
+              )
+            );
+            if (!querySnapshot2.empty){
+              querySnapshot2.forEach((doc) => {
+              const violationData = doc.data();
+              count30 += violationData.count30;
+            });
+            }
+            if(count30>1){
+              price=2000;//max limit price for violation
+             }
+          }
+         }
+
          const querySnapshot = await getDocs(
           query(
-              collection(db, 'Violation'),// I already done construct t table
+              collection(db, 'Violation'),// I already done construct violation table
               where('GPSnumber', '==', GPSserialnumber),
               where('driverID', '==', driverid),
               orderBy('time', 'desc'), // Order by time in descending order 
@@ -120,52 +182,43 @@ export const monitorUnits = (sess, fetchInterval) => {
            if (!querySnapshot.empty){//condition distance<30 if yes new condition about if same data no give مخالفه if yes new condition يكون about time 30-45 
             console.log('helo');
              const lastViolation = querySnapshot.docs[0];    
-             console.log(lastViolation.data().position.latitude);// make suuuuuuuuuuuuuuuuure
+             console.log(lastViolation.data().position.latitude);
              const distance = haversineDistance(lastViolation.data().position.latitude, lastViolation.data().position.longitude, pos.y, pos.x);
              console.log("distance before condition:",distance);
              console.log('retrived in query to in 30km:',lastViolation.data().GPSnumber, ' have :', lastViolation.data().location,' pri:',lastViolation.data().time , 'position lat:',lastViolation.data().position.latitude, ' long:',lastViolation.data().position.longitude);
              if (distance>30){
                console.log("more than 30km",distance);
-               await storeViolation(driverid,GPSserialnumber, location ,position, driverSpeed,maxSpeed, newViolationTime,price);
+               await storeViolation(ViolationID,driverid,GPSserialnumber, location ,position, driverSpeed,maxSpeed, newViolationTime,price,count50,count30);
+               await storeHistory(ViolationID,driverid,GPSserialnumber, Brand ,LicensePlate, Model,MotorcycleID, Type);
              }
              else{
-              const tenMinutesInSeconds = 1 * 60;
+              const tenMinutesInSeconds = 10 * 60; //10*60
               console.log('less than 30km');
               if (!areDatesEqual(lastViolation.data().time, newViolationTime)) {
                 console.log('not same date');
-               await storeViolation(driverid,GPSserialnumber, location ,position, driverSpeed,maxSpeed, newViolationTime,price);
+               await storeViolation(ViolationID,driverid,GPSserialnumber, location ,position, driverSpeed,maxSpeed, newViolationTime,price,count50,count30);
+               await storeHistory(ViolationID,driverid,GPSserialnumber, Brand ,LicensePlate, Model,MotorcycleID, Type);
+
               }
               else if(newViolationTime-lastViolation.data().time>tenMinutesInSeconds){
                 console.log('exceed 10 min');
-                await storeViolation(driverid,GPSserialnumber, location ,position, driverSpeed,maxSpeed, newViolationTime,price);
+                await storeViolation(ViolationID,driverid,GPSserialnumber, location ,position, driverSpeed,maxSpeed, newViolationTime,price,count50,count30);
+                await storeHistory(ViolationID,driverid,GPSserialnumber, Brand ,LicensePlate, Model,MotorcycleID, Type);
               }
               else{
                 console.log('same date and did not exceed 10 min');//i will remove this else 
-              }
-     
-            }
-
+              } }
            }
-
            else{
-             await storeViolation(driverid,GPSserialnumber, location ,position, driverSpeed,maxSpeed,newViolationTime,price);
+             await storeViolation(ViolationID,driverid,GPSserialnumber, location ,position, driverSpeed,maxSpeed,newViolationTime,price,count50,count30);
+             await storeHistory(ViolationID,driverid,GPSserialnumber, Brand ,LicensePlate, Model,MotorcycleID, Type);
             console.log('no violation for this GPS before.');
-           }
-          
-           }
-
-          } 
-
-          
-        }//end of loop check speed>max
-
-        // else {
-        //   console.log("No need to store violation for", unit.getName());
-        // }
-      
+           } }} } 
+         }//end of loop check speed>max     
       }}
       };
   };
+
   const areDatesEqual = (unixTime1, unixTime2) => {
     // Convert UNIX timestamps (in seconds) to Date objects
     const date1 = new Date(unixTime1 * 1000);
@@ -196,10 +249,11 @@ export const monitorUnits = (sess, fetchInterval) => {
 }
 
 const generateViolationId = () => {
-  const min = 1000000000; // Minimum 10-digit number
-  const max = 9999999999; // Maximum 10-digit number
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const min = 100000000; 
+  const max = 999999999; 
+  const randomNineDigits = Math.floor(Math.random() * (max - min + 1)) + min;
+  
+  return `1${randomNineDigits}`;
 };
 
 
@@ -235,9 +289,8 @@ const generateViolationId = () => {
     return price;
   };
 
-  const storeViolation = async (driverid,GPSnumber, location,position, speed,maxSpeed, time,price) => {
+  const storeViolation = async (ViolationID,driverid,GPSnumber, location,position, speed,maxSpeed, time,price,count50,count30) => {
     try {
-      const ViolationID = generateViolationId(); // Generate a unique ID for the violation
       await addDoc(collection(db, 'Violation'), {
         violationID:ViolationID,
         driverID:driverid,
@@ -249,6 +302,8 @@ const generateViolationId = () => {
         price:price,
         time: time,
         timestamp: Timestamp.now(),
+        count50:count50,
+        count30:count30,
       });
       console.log('Violation stored successfully.');
     } catch (e) {
@@ -256,6 +311,171 @@ const generateViolationId = () => {
       console.log('Failed to store violation.');
     }
    };
+
+   const storeHistory = async (id,driverid,GPSserialnumber, Brand ,LicensePlate, Model,MotorcycleID, Type) => {
+    try {
+      await addDoc(collection(db, 'History'), {
+        ID:id, //crash or violation id
+        Driverid:driverid,
+        GPSnumber: GPSserialnumber,
+        Brand:Brand,
+        LicensePlate: LicensePlate,
+        Model: Model,
+        MotorcycleID:MotorcycleID,
+        Type:Type,
+      });
+      console.log('history stored successfully.');
+    } catch (e) {
+      console.error('Error storing history:', e);
+      console.log('Failed to store history.');
+    }
+   };
+
+
+   //crash function 
+
+   const processUnits2 = async (units) => {
+    for (const unit of units) { 
+      console.log("crashh");
+        const id=unit.getId();
+        console.log("id:",id); 
+        const pos = unit.getPosition();
+        console.log(pos);
+        if(pos){
+         const GPSserialnumber = unit.getName();
+         console.log('crash gpsnum:',GPSserialnumber);
+         const driverSpeed = pos.s;
+         console.log('driverspeed:',driverSpeed);
+         const driverQuerySnapshot1 = await getDocs(
+            query(
+                collection(db, 'Driver'), 
+                where('GPSnumber', '==', GPSserialnumber)
+            )
+         );
+         if (driverQuerySnapshot1.empty) {
+          console.log(`No driver found for GPS number: ${GPSserialnumber}. Skipping.`);
+      }
+         else{
+          const driverid = driverQuerySnapshot1.docs[0].data().DriverID;
+          console.log('DriverID:', driverid);
+          const querySnapshot3 = await getDocs(
+            query(
+              collection(db, 'Motorcycle'),
+              where('GPSnumber', '==', GPSserialnumber)
+            )
+          );
+          if (!querySnapshot3.empty) {
+       const Brand = querySnapshot3.docs[0].data().Brand;
+       const LicensePlate = querySnapshot3.docs[0].data().LicensePlate;
+       const Model = querySnapshot3.docs[0].data().Model;
+       const MotorcycleID = querySnapshot3.docs[0].data().MotorcycleID;
+       const Type = querySnapshot3.docs[0].data().Type;
+       const CrashID = generateCrashId(); 
+         const newcrashTime=unit.getLastMessage().rt;//time of this unit read
+         console.log('current crashtime:',newcrashTime);
+         
+            const position = { longitude:pos.x , latitude: pos.y }; 
+            console.log(position);
+            // Fetch location using GIS asynchronously
+            const location = await new Promise((resolve) => {
+              window.wialon.util.Gis.getLocations([{ lon: pos.x, lat: pos.y }], (code, address) => {
+                  resolve(address[0]); // Resolve the promise with the first address
+              });
+           });
+           console.log(location);
+           const recentSpeeds = {}; // Dictionary to store recent speeds for each unit
+           const to = newcrashTime; // Current time
+           const from = to - 100; // Check the last 10 seconds for relevant messages
+           console.log("from: ",from," to: ",to); 
+           var ml = sess.getMessagesLoader();
+           ml.loadInterval(id, from, to, 0, 0, 100, async function (code, data) {
+            if ( code || !data.messages) { 
+                console.log(data.messages);
+                console.log(data);
+                console.log("Insufficient data for acceleration check");
+            }
+          else{
+            const messages = data.messages;
+            console.log("messages: ",messages);
+            // update recentSpeeds for the unit
+            recentSpeeds[GPSserialnumber] = messages.filter(message => message.pos !== null).map(message => ({
+                speed: message.pos.s, 
+                time: message.t
+            }));
+            console.log("recentarray: ",recentSpeeds);
+            if (recentSpeeds[GPSserialnumber].length < 2)  {
+              console.log("less that 2 reads after filter");
+              }
+            else{
+            // Calculate deceleration over the retrieved messages
+            for (let i = 1; i < recentSpeeds[GPSserialnumber].length; i++) { // In recentSpeds 1=25 first, 2=15 last ... 
+                const prevread = recentSpeeds[GPSserialnumber][i - 1]; //25
+                const currread = recentSpeeds[GPSserialnumber][i]; //15
+                const deltaViolocity = currread.speed - prevread.speed;
+                const deltaTime = currread.time - prevread.time;
+                const acceleration = deltaViolocity / deltaTime;
+                console.log(acceleration);
+    
+                // Check if deceleration indicates crash
+                if (acceleration <= -1) {  // Emergency deceleration 
+                    console.log("Potential crash detected for :", GPSserialnumber, "Acceleration:", acceleration);
+                    const starttime = newcrashTime - 5 * 60; // crashes in 5 min before.  
+                    const endtime = newcrashTime;
+                    const querySnapshot = await getDocs(
+                      query(
+                        collection(db, 'Crash'),
+                        where('GPSnumber', '==', GPSserialnumber),
+                        where('driverID', '==', driverid),
+                        where('time', '>=', starttime),
+                        where('time', '<=', endtime)
+                      )
+                    );
+                    if (!querySnapshot.empty){
+                       console.log("there is crash is last 5 min.");
+                    }
+                    else{
+                        await storeCrash(CrashID,driverid,GPSserialnumber, location ,position, driverSpeed, newcrashTime);
+                        await storeHistory(CrashID,driverid,GPSserialnumber, Brand ,LicensePlate, Model,MotorcycleID, Type);
+
+                    }
+                    break; //exit when crash is detected.
+                }
+            }}}
+        });
+
+        }} 
+      }
+      };
+};
+
+
+const storeCrash = async (CrashID,driverid, GPSnumber, location, position, speed, time) => {
+    try {
+      await addDoc(collection(db, 'Crash'), {
+        crashID:CrashID,
+        driverID:driverid,
+        GPSnumber: GPSnumber,
+        location:location,
+        position: position,
+        driverSpeed: speed,
+        time: time,
+        timestamp: Timestamp.now(),
+        Status:"Rejected",
+      });
+      console.log('crash stored successfully.');
+    } catch (e) {
+      console.error('Error storing crash:', e);
+      console.log('Failed to store crash.');
+    }
+   };
+
+   const generateCrashId = () => {
+    const min = 100000000; 
+    const max = 999999999; 
+    const randomNineDigits = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    return `2${randomNineDigits}`;
+  };
 
 
   // Set up the interval
@@ -266,6 +486,7 @@ const generateViolationId = () => {
 
   // Return the cleanup function for clearing the interval
   return () => clearInterval(intervalId);
+
 
 
 };
