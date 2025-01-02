ac6fea6d78ce4ec8d4a2d92786199d9085a65c07
RawanSaud23 <srawanalsaif@gmail.com>
2025-01-02 03:56:30 +0300

    styling :)

--- sair/src/Maincomponent/GDT/GDTHome.jsx
+++ sair/src/Maincomponent/GDT/GDTHome.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import homeBackground from '../../images/homebackground7.png';
-import Vision from '../../images/Vision.png';
-import Mision from '../../images/Mision.png'; 
 import Header from './GDTHeader';
-import s from "../../css/EmployerHome.module.css";
+import '../../css/EmployerHome.module.css';
 import '../../css/CustomModal.css';
 
 const GDTHome = () => {
-  const styles = {
+  const pageStyles = {
     backgroundImage: `url(${homeBackground})`,
-    backgroundSize: '1000px', // Change to 'cover' for better scaling
-    backgroundPosition: 'right', // Centers the image
-    height: '100vh', // Sets the height to full viewport height
-    width: '100%', // Ensures it takes the full width
+    backgroundSize: '1000px', 
+    backgroundPosition: 'right', 
+    height: '100vh',
+    width: '100%', 
     backgroundRepeat: 'no-repeat',
-    };
+  };
+
+  const h1WrapperStyles = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 'calc(100vh - 50px)', 
+  };
 
   return (
-    <div style={styles}> 
-      <Header active="gdthome" /> 
-      <h1>will be done in sprint 5</h1>
+    <div style={pageStyles}>
+      <Header active="gdthome" />
+      <div style={h1WrapperStyles}>
+        <h1>GDT dashbord will be done in sprint 5</h1>
+      </div>
     </div>
   );
 };
