9a56c8376e48fe0888747ff7e5cdac505306e4ae
Ghalaalaskar <ghalaturki555@gmail.com>
2025-01-10 15:18:14 +0300

    Adding the sentences and modify some things

--- sair/src/Maincomponent/GDT/GDTViolationgeneral.jsx
+++ sair/src/Maincomponent/GDT/GDTViolationgeneral.jsx
@@ -1069,69 +1069,31 @@ const ViolationGeneral = () => {
                 </p>
               </div>
               <div>
-                <h3
-                  style={{
-                    color: "#059855",
-                    fontWeight: "bold",
-                    fontSize: "20px",
-                  }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="35"
-                    height="35"
-                    style={{ marginBottom: "-5px", marginRight: "10px" }}
-                    color="#059855"
-                    fill="none"
-                  >
-                    <path
-                      d="M20.016 2C18.9026 2 18 4.68629 18 8H20.016C20.9876 8 21.4734 8 21.7741 7.66455C22.0749 7.32909 22.0225 6.88733 21.9178 6.00381C21.6414 3.67143 20.8943 2 20.016 2Z"
-                      stroke="currentColor"
-                      stroke-width="1.5"
-                    />
-                    <path
-                      d="M18 8.05426V18.6458C18 20.1575 18 20.9133 17.538 21.2108C16.7831 21.6971 15.6161 20.6774 15.0291 20.3073C14.5441 20.0014 14.3017 19.8485 14.0325 19.8397C13.7417 19.8301 13.4949 19.9768 12.9709 20.3073L11.06 21.5124C10.5445 21.8374 10.2868 22 10 22C9.71321 22 9.45546 21.8374 8.94 21.5124L7.02913 20.3073C6.54415 20.0014 6.30166 19.8485 6.03253 19.8397C5.74172 19.8301 5.49493 19.9768 4.97087 20.3073C4.38395 20.6774 3.21687 21.6971 2.46195 21.2108C2 20.9133 2 20.1575 2 18.6458V8.05426C2 5.20025 2 3.77325 2.87868 2.88663C3.75736 2 5.17157 2 8 2H20"
-                      stroke="currentColor"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M10 8C8.89543 8 8 8.67157 8 9.5C8 10.3284 8.89543 11 10 11C11.1046 11 12 11.6716 12 12.5C12 13.3284 11.1046 14 10 14M10 8C10.8708 8 11.6116 8.4174 11.8862 9M10 8V7M10 14C9.12919 14 8.38836 13.5826 8.1138 13M10 14V15"
-                      stroke="currentColor"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                    />
-                  </svg>
-                  Violation Amount
-                </h3>
-                <p style={{ fontSize: "18px", marginLeft: "45px" }}>
-                  {currentViolation.price} SAR
-                </p>
-                {(currentViolation.count30 > 0 ||
-                  currentViolation.count50 > 0) && (
-                  <p
-                    style={{
-                      fontSize: "15px",
-                      marginLeft: "45px",
-                      color: "#FF0000",
-                    }}
-                  >
-                    According to the General Department of Traffic regulations,
-                    This speed violation is considered reckless and marks the
-                    driver's{" "}
-                    <strong>
-                      {getOrdinal(
-                        currentViolation.count30 > 0
-                          ? currentViolation.count30
-                          : currentViolation.count50
-                      )}
-                    </strong>{" "}
-                    offense. <br /> As a result, the penalty amount has been
-                    increased.
-                  </p>
-                )}
+              <h3 style={{color:"#059855", fontWeight:'bold',fontSize:'20px' }}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="35" height="35" style={{marginBottom:'-5px', marginRight:'10px'}} color="#059855" fill="none">
+    <path d="M20.016 2C18.9026 2 18 4.68629 18 8H20.016C20.9876 8 21.4734 8 21.7741 7.66455C22.0749 7.32909 22.0225 6.88733 21.9178 6.00381C21.6414 3.67143 20.8943 2 20.016 2Z" stroke="currentColor" stroke-width="1.5" />
+    <path d="M18 8.05426V18.6458C18 20.1575 18 20.9133 17.538 21.2108C16.7831 21.6971 15.6161 20.6774 15.0291 20.3073C14.5441 20.0014 14.3017 19.8485 14.0325 19.8397C13.7417 19.8301 13.4949 19.9768 12.9709 20.3073L11.06 21.5124C10.5445 21.8374 10.2868 22 10 22C9.71321 22 9.45546 21.8374 8.94 21.5124L7.02913 20.3073C6.54415 20.0014 6.30166 19.8485 6.03253 19.8397C5.74172 19.8301 5.49493 19.9768 4.97087 20.3073C4.38395 20.6774 3.21687 21.6971 2.46195 21.2108C2 20.9133 2 20.1575 2 18.6458V8.05426C2 5.20025 2 3.77325 2.87868 2.88663C3.75736 2 5.17157 2 8 2H20" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M10 8C8.89543 8 8 8.67157 8 9.5C8 10.3284 8.89543 11 10 11C11.1046 11 12 11.6716 12 12.5C12 13.3284 11.1046 14 10 14M10 8C10.8708 8 11.6116 8.4174 11.8862 9M10 8V7M10 14C9.12919 14 8.38836 13.5826 8.1138 13M10 14V15" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+    </svg>Violation Amount</h3>
+<p style={{fontSize:'18px', marginLeft:'45px'}}>{currentViolation.price} SAR</p>
+<p style={{marginLeft: '45px', color: 'grey' }}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none" style={{marginBottom:'-5px'}}>
+        <circle cx="12" cy="12" r="10" stroke="red" strokeWidth="1.5" />
+        <path d="M11.992 15H12.001" stroke="red" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M12 12L12 8" stroke="red" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+    According to the General Department of Traffic regulations, this speed violation is considered reckless and marks the driver's <strong>{getOrdinal(currentViolation.count30 > 0 ? currentViolation.count30 : currentViolation.count50)}</strong> offense.
+    { (currentViolation.count30 > 1 || currentViolation.count50 > 1) && (
+        <p style={{marginLeft: '45px', color: 'grey' }}><span style={{marginLeft:'-12px'}}> As a result, the penalty amount has been increased.</span></p>
+    )}
+</p>
+<p style={{marginLeft: '45px', color: 'grey' }}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none" style={{marginBottom:'-5px'}}>
+    <circle cx="12" cy="12" r="10" stroke="red" stroke-width="1.5" />
+    <path d="M11.992 15H12.001" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M12 12L12 8" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
+   Disclaimer: This fee is an estimated amount, calculated based on the executive regulations issued under ministerial decision No.2249, Article 115.
+  </p>
+               
               </div>
               <div>
                 <h3
